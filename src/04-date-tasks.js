19c66d09db318c2ec3117a4ec44842784ebb831c
IvanMakarishchev <denis.flank@gmail.com>
2022-07-24 21:51:26 +0300

    feat: finish date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return (new Date(date.getFullYear() + 1, 1, 1).getTime() - date.getTime()) % 316224e5 === 0;
 }
 
 
@@ -73,8 +73,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return `${new Date(endDate - startDate).toLocaleTimeString('en-GB', { timeZone: 'GMT' })}.${(endDate - startDate).toString().slice(-3)}`;
 }
 
 
@@ -94,8 +94,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = +date.toLocaleTimeString('en-GB', { timeZone: 'GMT', hour: 'numeric', hour12: true }).match(/[0-9]+/g);
+  const minute = date.toLocaleTimeString('en-GB', { timeZone: 'GMT', minute: 'numeric' });
+  const hourDeg = hour * 30 + minute * 0.5;
+  const minuteDeg = minute * 6;
+  const handsDeg = hourDeg - minuteDeg > 180
+    ? 360 - Math.abs(hourDeg - minuteDeg)
+    : Math.abs(hourDeg - minuteDeg);
+  return handsDeg * (Math.PI / 180);
 }
 
 
