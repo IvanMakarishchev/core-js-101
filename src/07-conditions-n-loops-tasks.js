6bd681b6097e223bffb77532f20e58c87d319759
IvanMakarishchev <denis.flank@gmail.com>
2022-07-25 03:00:30 +0300

    feat: making

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -316,7 +316,7 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str*/) {
+function isBracketsBalanced(/* str */) {
   throw new Error('Not implemented');
 }
 
